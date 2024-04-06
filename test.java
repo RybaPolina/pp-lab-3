a91e0fe3ffa07d1ed0565aa85815144a2e936129
Polina Rybachuk <polia6v@gmail.com>
2024-04-06 09:58:35 +0200

    zad3

--- test.java
+++ test.java
@@ -1,19 +1,22 @@
 import Geometry.Point;
-import Geometry.Circle;
+import Geometry.ColoredCircle;
 
 public class test {
     public static void main(String[] args) {
         Point center = new Point(1.0, 2.0);
 
-        center.setX(10.0);
-        center.setY(-5.0);
+        center.setX(3.0);
+        center.setY(2.0);
 
         double radius = 10.0;
-        Circle circle = new Circle(center, radius);
+        String color = "Zielony";
+
+        ColoredCircle colorcircle = new ColoredCircle(center, radius, color);
         
-        System.out.println("Punkt środkowy: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
-        System.out.println("Promień: " + circle.getRadius());
-        System.out.println("Obwód: " + circle.calculatePerimeter());
+        System.out.println("Punkt środkowy: (" + colorcircle.getCenter().getX() + ", " + colorcircle.getCenter().getY() + ")");
+        System.out.println("Promień: " + colorcircle.getRadius());
+        System.out.println("Obwód: " + colorcircle.calculatePerimeter());
+        System.out.println("Kolor:" +colorcircle.getColor());
     }
 
 }
