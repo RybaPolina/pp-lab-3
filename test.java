b2b3d2bdb07d0918de7e9e2ac315318b5c48c6fa
Polina Rybachuk <polia6v@gmail.com>
2024-04-06 09:05:07 +0200

    zad1

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        Point punkt = new Point(0, 0);
+
+        punkt.setX(10.0);
+        punkt.setY(-5.0);
+
+        System.out.println(punkt);
+      }
+}
