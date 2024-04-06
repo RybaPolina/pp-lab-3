da10c9e3707b47412f1eb87c11a95c0abbbd3206
Polina Rybachuk <polia6v@gmail.com>
2024-04-06 09:35:11 +0200

    zad2

--- test.java
+++ test.java
@@ -1,10 +1,19 @@
+import Geometry.Point;
+import Geometry.Circle;
+
 public class test {
     public static void main(String[] args) {
-        Point punkt = new Point(0, 0);
+        Point center = new Point(1.0, 2.0);
 
-        punkt.setX(10.0);
-        punkt.setY(-5.0);
+        center.setX(10.0);
+        center.setY(-5.0);
 
-        System.out.println(punkt);
-      }
+        double radius = 10.0;
+        Circle circle = new Circle(center, radius);
+        
+        System.out.println("Punkt środkowy: (" + circle.getCenter().getX() + ", " + circle.getCenter().getY() + ")");
+        System.out.println("Promień: " + circle.getRadius());
+        System.out.println("Obwód: " + circle.calculatePerimeter());
+    }
+
 }
